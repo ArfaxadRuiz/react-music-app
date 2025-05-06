adc14eae82d7ae922e8a82ec59a316863f89538d
ArfaxadRuiz <iron_arfa@hotmail.com>
2025-05-06 10:09:19 -0600

    se cambia estructura de App.js de funcion a clase para usar metodos de forma mas sencilla

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from "./components/Header";
 import Song from './components/Song';
 import './App.css';
 
-function App() {
-  return (
 
-    <div className="App">
-      <Header />
-      <Song titulo="Bohemian Rhapsody" artista="Queen" duracion="5:55" />
-      <Song titulo="Imagine" artista="John Lennon" duracion="3:12" />
-      <Song titulo="Six" artista="All That Remains" duracion="3:06" />
-    </div>
 
-  );
+class App extends Component{
+  componentDidMount() {
+    console.log('La aplicación se ha cargado correctamente.');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Song titulo="Bohemian Rhapsody" artista="Queen" duracion="5:55" />
+        <Song titulo="Imagine" artista="John Lennon" duracion="3:12" />
+        <Song titulo="Six" artista="All That Remains" duracion="3:06" />
+      </div>
+    );
+  }
+
 }
 
 export default App;
