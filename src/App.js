cc636f3f4b542c10cda54d4a4b3468492246b2f7
ArfaxadRuiz <iron_arfa@hotmail.com>
2025-05-08 08:18:04 -0600

    se realizan cambios para usar el useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Header from "./components/Header";
 import Song from './components/Song';
 import './App.css';
 
 
 
-class App extends Component{
-  componentDidMount() {
+function App() {
+  
+  useEffect(() => {
     console.log('La aplicación se ha cargado correctamente.');
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Song titulo="Bohemian Rhapsody" artista="Queen" duracion="5:55" />
-        <Song titulo="Imagine" artista="John Lennon" duracion="3:12" />
-        <Song titulo="Six" artista="All That Remains" duracion="3:06" />
-      </div>
-    );
-  }
-
+  return (
+    <div className="App">
+      <Header />
+      <Song titulo="Bohemian Rhapsody" artista="Queen" duracion="5:55" />
+      <Song titulo="Imagine" artista="John Lennon" duracion="3:12" />
+      <Song titulo="Six" artista="All That Remains" duracion="3:06" />
+    </div>
+  );
 }
 
 export default App;
